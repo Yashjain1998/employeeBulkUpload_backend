97df735d581e8e5949f3290f258b60e4e2c4ef76
Yash jain <126048815+Yashjain1998@users.noreply.github.com>
2025-01-19 17:31:00 +0000

    fix the multer

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -8,16 +8,23 @@ const {
 } = require("../controllers/employeeController");
 const multer = require("multer");
 
+// Ensure the /tmp/uploads directory exists
+const uploadPath = path.join("/tmp", "uploads");
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
+
 // Multer configuration
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: uploadPath });
 const router = express.Router();
 
 router.get("/", getAllEmployees);
 router.post("/", addEmployee1);
 
 // File upload routes
-// router.post("/upload-csv", upload.single("file"), uploadCSV1);
-// router.post("/upload-xlsx", upload.single("file"), uploadXLSX);
+router.post("/upload-csv", upload.single("file"), uploadCSV1);
+router.post("/upload-xlsx", upload.single("file"), uploadXLSX);
 
 // Download CSV route
 router.get("/download-csv", downloadCSV1);
