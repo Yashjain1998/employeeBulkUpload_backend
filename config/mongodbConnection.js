778321791e24e60a00e8fab64997fce881db7180
Yash jain <126048815+Yashjain1998@users.noreply.github.com>
2025-01-19 17:39:58 +0000

    change

--- config/mongodbConnection.js
+++ config/mongodbConnection.js
@@ -4,11 +4,10 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      // process.env.MONGO_URI
-      "mongodb+srv://Test_db:Test_db@cluster0.duvmrrh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      process.env.MONGO_URI
       , {
-    
-    });
+
+      });
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('MongoDB connection error:', error);
