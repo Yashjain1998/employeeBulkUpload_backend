66aac9052b2b20225ae1a3aa86286862395cbcc5
Yash jain <126048815+Yashjain1998@users.noreply.github.com>
2025-01-20 13:42:48 +0000

    add cors connect

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const employeeRoutes = require("./routes/employeeRoutes");
 const payrollRoutes = require("./routes/payrollRoutes");
+const cors = require('cors');
 
 // Initialize express app
 const app = express();
 
 // Middleware
+// Default configuration
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
